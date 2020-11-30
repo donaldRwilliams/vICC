31567920ca598c5180ee1a82af0eb2dbdb091938
Donald R. Williams <brycewayne03.29@gmail.com>
2020-11-30 05:19:50 -0800

    add data and description

---
+++ R/datasets.R
@@ -0,0 +1,32 @@
+#' @title Data: Flanker Task data from \insertCite{hedge2018reliability;textual}{vICC}.
+#'
+#' @description A dataset containing 33660 rows and 7 columns.
+#'
+#' \itemize{
+#' \item Block
+#' \item Trial number
+#' \item Arrow direction (1=left, 2=right)
+#' \item Condition (0 = congruent, 1=neutral, 2=incongruent)
+#' \item Correct (1) or incorrect (0)
+#' \item Reaction time (seconds)
+#' }
+#'
+#' @note
+#' Reaction times less than 0.20 and greater than 2 seconds were removed.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @name flanker
+#'
+#' @usage data("flanker")
+#'
+#' @references
+#'
+#' \insertAllCited{}
+#'
+#' @format A dataframe 33660 rows and 7 columns.
+NULL
+
+
